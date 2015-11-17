e8725b250c96b80c9a700e546855cf5fd72f81b3
mabdylon <mathieu.barat@gmail.com>
2015-11-17 21:48:59 +0100

    Amelioration Moisture servo, ajout min max dynamique et refactor du code

--- arduino_moisture_servo/arduino_moisture_servo.ino
+++ arduino_moisture_servo/arduino_moisture_servo.ino
@@ -10,47 +10,78 @@ VarSpeedServo servoAlert;
 int mappedMoisture = 0;
 int lastMappedMoisture = 0;
 int digitalMoisture = 0;
-int lastDigitalMoisture = 0;
+int analogMoisture = 600;
+int maxMappedMoisture = 950;
+int minMappedMoisture = 350;
+int amountNeededToMove = 5;
+boolean DEBUG = true;
 
-ISR(WDT_vect) { Sleepy::watchdogEvent(); }
+ISR(WDT_vect) {
+  Sleepy::watchdogEvent();
+}
+
+void debugOutput() {
+  if (DEBUG) {
+    Serial.print("Last mapped moisture : ");
+    Serial.println(lastMappedMoisture);
+    Serial.print("MappedMoisture : ");
+    Serial.println(mappedMoisture);
+    Serial.print("Analog Moisture : ");
+    Serial.println(analogMoisture);
+    Serial.print("Digital Moisture : ");
+    Serial.println(digitalMoisture);
+    Serial.println("-----------------------");
+  }
+}
+
+void readInputs() {
+  analogMoisture = analogRead(pinMoistureAnalog);
+  digitalMoisture = digitalRead(pinMoistureDigital);
+}
+
+void ifExceedModifyMinMax() {
+  if (analogMoisture < minMappedMoisture) {
+    minMappedMoisture = analogMoisture;
+  }
+  if (analogMoisture > maxMappedMoisture) {
+    maxMappedMoisture = analogMoisture;
+  }
+}
+
+void computeMappedMoisture() {
+  mappedMoisture = map(analogMoisture, minMappedMoisture, maxMappedMoisture, 0, 180);
+}
+
+void ifDiffIsEnoughMoveServo() {
+  if (abs(lastMappedMoisture - mappedMoisture) > amountNeededToMove) {
+    servoMesure.write(mappedMoisture, 20, true);
+    lastMappedMoisture = mappedMoisture;
+    int flagAlertPosition = (digitalMoisture == 0 ? 90 : 180);
+    servoAlert.write(flagAlertPosition, 50, true);
+  }
+}
 
 void setup() {
-  pinMode(pinMoistureAnalog,INPUT);
-  pinMode(pinMoistureDigital,INPUT);
+  pinMode(pinMoistureAnalog, INPUT);
+  pinMode(pinMoistureDigital, INPUT);
   servoMesure.attach(pinSerovMesure);
   servoAlert.attach(pinSerovAlert);
   Serial.begin(9600);
 }
 
 void loop() {
-  int analogMoisture = analogRead(pinMoistureAnalog);
-  digitalMoisture = digitalRead(pinMoistureDigital);
-  mappedMoisture = map(analogMoisture, 300, 1023, 0, 180);
-  
-  Serial.print("Last mapped moisture : ");
-  Serial.println(lastMappedMoisture);
-  Serial.print("MappedMoisture : ");
-  Serial.println(mappedMoisture);
-  
-  
-  if(abs(lastMappedMoisture - mappedMoisture) > 3) {
-    servoMesure.write(mappedMoisture, 20, true);
-    lastMappedMoisture = mappedMoisture;  
-    if(digitalMoisture != lastDigitalMoisture) {
-       int flagAlertPosition = (digitalMoisture == 0 ? 90 : 180);
-       servoAlert.write(flagAlertPosition, 40, true);
-       lastDigitalMoisture = digitalMoisture;
-    }
-  }
 
-  Serial.print("Analog Moisture : ");
-  Serial.println(analogMoisture);
-  Serial.print("Digital Moisture : ");
-  Serial.println(digitalMoisture);
-  Serial.println("-----------------------");
-  
-  Sleepy::loseSomeTime(5000);
-  
-  
+  readInputs();
+  ifExceedModifyMinMax();
+  computeMappedMoisture();
+  debugOutput();
+  ifDiffIsEnoughMoveServo();
+
+
+  Sleepy::loseSomeTime(3000);
+
+
 }
+
+
 
